ae3462462d3e2489cc02d903191fd28533cbb18d
JeremiahID <Jeremiahefe.je@gmail.com>
2024-10-16 17:21:46 +0100

    first commit

---
+++ Js/app.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+/*
+
+// this is the Anagram function
+function areAnagrams(str1, str2) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanString = (str) =>
+      str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+  
+    return cleanString(str1) === cleanString(str2);
+  }
+  
+  // Test cases
+  console.log(areAnagrams('listen', 'silent')); // true
+  console.log(areAnagrams('anagram', 'nagaram')); // true
+  console.log(areAnagrams('hello', 'world')); // false
+  console.log(areAnagrams('School master', 'The classroom')); // true
+  
+  */
