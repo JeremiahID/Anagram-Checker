49f49b87d75a4c12d9a2857d7dd532c1032aa77b
JeremiahID <Jeremiahefe.je@gmail.com>
2024-10-17 20:20:18 +0100

    testing anagram

--- Js/app.js
+++ Js/app.js
@@ -1,49 +1,42 @@
-const FisrtWord = document.querySelector('#wordOne').toString();
-const SecondWord = document.querySelector('#wordTwo').toString();
+const FisrtWord = document.querySelector('#wordOne');
+const SecondWord = document.querySelector('#wordTwo');
 const WordChecker = document.querySelector('#wordChecker');
 const ErrorMsg = document.querySelector('#errorMsg');
 
 WordChecker.addEventListener('click', ()=>{
-  console.log(typeof FisrtWord);
-  console.log(typeof SecondWord);
-
-  // a function to check the Anagram
-  function Anagrams(str1, str2) {
-    
-    // Clean and sort the strings
-    const cleanString = (str) =>
-      str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
-  
-    // Compare the cleaned strings
-    return cleanString(str1) === cleanString(str2);
-  }
-
-
 
   // error mesage for empty fields
-  if(FisrtWord === '' || SecondWord === ''){
+  if(FisrtWord.value === '' || SecondWord.value === ''){
     ErrorMsg.style.display = "block";
 
   } else {
     ErrorMsg.style.display = "none";
 
     // passing the input via the function parameters
-    if(Anagrams(FisrtWord, SecondWord)){
-      console.log('test')
-      alert('True')
+    if(Anagrams(FisrtWord.toString(), SecondWord.toString())){
+      console.log('working');
+      console.log(typeof FisrtWord);
+      console.log(typeof SecondWord);
     } else {
       console.log('not working')
-      alert('False')
     }
 
   }
 
   FisrtWord.value = '';
   SecondWord.value = '';
-  
-
-
 
 
 })
 
+
+
+// a function to check the Anagram
+function Anagrams(str1, str2) {
+  // Clean and sort the strings
+  const cleanString = (str) =>
+    str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+
+  // Compare the cleaned strings
+  return cleanString(str1) === cleanString(str2);
+}
